12f9d5359b663667d6a427c6cfc6dd583978655b
Rohit Kalgude <rohitkalgude860@gmail.com>
2025-07-17 17:07:34 +0530

    update

---
+++ ColorGenrate.js
@@ -0,0 +1,39 @@
+const Generatebtn = document.getElementById("generate-btn");
+const Palettecontainer = document.getElementsByClassName("palette-container");
+const Colorbox = document.querySelectorAll(".color-box");
+
+Generatebtn.addEventListener("click", generatepalette);
+
+function generatepalette() {
+  const colors = [];
+
+  for (let i = 0; i < Colorbox.length; i++) {
+    colors.push(generaterandomcolor());
+  }
+
+  updatepalettedisplay(colors);
+}
+
+function generaterandomcolor() {
+  const letter = "0123456789ABCDEF";
+  let color = "#";
+
+  for (let i = 0; i < 6; i++) {
+    color += letter[Math.floor(Math.random() * 16)];
+  }
+
+  return color;
+}
+
+function updatepalettedisplay(colors) {
+  Colorbox.forEach((box, index) => {
+    const color = colors[index];
+    const colorDiv = box.querySelector(".color");
+    const hexvalue = box.querySelector(".hex-value");
+
+    colorDiv.style.backgroundColor = color;
+    hexvalue.textContent = color;
+  });
+}
+
+generatepalette();
